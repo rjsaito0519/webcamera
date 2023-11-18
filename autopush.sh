5cd921f1288f368cb723544d42eacdd36def5191
ryuta519 <ryuta11111111@gmail.com>
2023-11-19 03:36:24 +0900

    Sun, Nov 19, 2023  3:36:12 AM

--- autopush.sh
+++ autopush.sh
@@ -2,5 +2,6 @@
 
 VAR=`date`
 git add .
+
 git commit -m "$VAR"
 git push origin main
--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,30 @@
 import tifffile
 import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-filepath = glob.glob("./*.tiff")
+isTest = 0
+y_offset = 2
+x_offset = 47
 
-image = tifffile.imread(filepath[0])
-tifffile.imwrite('temp.tiff', image)
+dir = "./raw_data/Monazu"
+save_dir = "./data/monazu"
+img_name = "*"
+filepath = glob.glob("{}/{}.tiff".format(dir, img_name))
+
+for path in filepath:
+    image = tifffile.imread(path)[y_offset:-1*(y_offset+1), x_offset:-1*(x_offset+1)]
+    # image = tifffile.imread(path)
+    if isTest:
+        print(path)
+        plt.imshow(image)
+        plt.show()
+    else:
+        save_name = os.path.splitext(os.path.basename(path))[0]
+        # n = len( glob.glob("{}/*.tiff".format(save_dir)) )
+        # save_name = "Am_{:0=4}".format( n+1 )
+        print(save_name)
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        tifffile.imwrite("{}/{}.tiff".format( save_dir, save_name ), image)
